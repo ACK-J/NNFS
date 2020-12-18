af60c4dec8238f8fe8217e7783b722502abc5288
ACK-J <60232273+ACK-J@users.noreply.github.com>
2020-12-18 06:39:23 -0800

    finished chapter 2

--- Chapter3_Adding_Layers/dense_layer.py
+++ Chapter3_Adding_Layers/dense_layer.py
@@ -1,15 +1,23 @@
 import numpy as np
 import nnfs
+from nnfs.datasets import spiral_data
 
 nnfs.init()
-
-print(np.random.randn(2, 3))
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         # Initialize weights and biases        rows   X   columns
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        #                      row X columns
         self.biases = np.zeros((1, n_neurons))
     def forward(self, inputs):
         # Calculate output values from inputs, weights and biases
-        pass
+        self.output = np.dot(inputs, self.weights) + self.biases
+#  Creating the dataset
+X, y = spiral_data(samples=100, classes=3)
+# First layer is a dense layer with 2 input features and 3 output featurs
+dense1 = Layer_Dense(2,3)
+# Make a forward pass
+dense1.forward(X)
+# Getting the output of the first few samples
+print(dense1.output[:5])
